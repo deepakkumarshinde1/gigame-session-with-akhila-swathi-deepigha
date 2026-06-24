be4c2f422a4c7f9d9f944412dfe1bd4e0e083cbd
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2026-06-24 07:04:25 +0530

    DAy-1

---
+++ js/hoisting.js
@@ -0,0 +1,21 @@
+// run this function
+funName(); // function call
+var a = 10;
+console.log(a);
+console.log("ndfsnd");
+console.log(1234);
+console.log(1234.7654);
+console.log(true);
+console.log("#$%@");
+
+console.log(a2); // temporal dead zone
+let a1;
+var a2 = 10;
+const a3 = 10;
+console.log(a1);
+
+// function
+function funName() {
+  // logic
+  console.log("I am a function");
+} // definition
---
+++ js/index.js
@@ -0,0 +1,27 @@
+// javascript variables
+// old
+var varName = "value";
+varName = 10; // work
+// new
+let varName1 = "value";
+varName1 = 10; // work
+const varName2 = "value";
+varName2 = 10; // error
+
+var $a; // work
+let $a; // work
+const $pi = 3.14; // error
+
+// access => hoisting js
+
+// 10 variables
+// 4 variables where default values are not assign
+// 6 variables where default values are assign
+// 8 function
+
+// 1 function initialization
+// 2 variable initialization ==> undefined
+// 3 variable values are assign
+
+var a; // undefined
+a = 10;
